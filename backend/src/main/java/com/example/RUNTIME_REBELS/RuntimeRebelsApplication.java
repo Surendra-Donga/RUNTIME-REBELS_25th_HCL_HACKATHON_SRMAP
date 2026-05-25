b3e418c055f8d99e6a2aa9aafbfe230b7c52ad0a
jayanth-chepuri <jayanthnagagandhi_chepuri@srmap.edu.in>
2026-05-25 11:22:54 +0530

    first commit

---
+++ backend/src/main/java/com/example/RUNTIME_REBELS/RuntimeRebelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RUNTIME_REBELS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntimeRebelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntimeRebelsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/RUNTIME_REBELS/RuntimeRebelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RUNTIME_REBELS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntimeRebelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
