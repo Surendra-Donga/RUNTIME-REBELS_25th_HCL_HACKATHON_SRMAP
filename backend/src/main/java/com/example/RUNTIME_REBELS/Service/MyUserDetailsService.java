c4e32538873b415d362464b6a4061781bc96a89d
Vamsi krishna Edara <vamsikrishnaeedara09@gmail.com>
2026-05-25 11:54:30 +0530

    implemented security

---
+++ backend/src/main/java/com/example/RUNTIME_REBELS/Service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.RUNTIME_REBELS.Service;
+
+import com.example.RUNTIME_REBELS.Models.Users;
+import com.example.RUNTIME_REBELS.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
